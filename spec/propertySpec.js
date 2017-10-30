fa87fe62c0edfef3c8f58d60b72a0b077f8ff304
Tim Jones <timjones10@hotmail.com>
2017-10-30 15:14:27 +0000

    Description and price properties added to property object

--- spec/propertySpec.js
+++ spec/propertySpec.js
@@ -4,10 +4,21 @@ describe("Property", function() {
 
  beforeEach(function() {
    var title = "Cottage";
-  property = new Property(title);
+   var description = "Two up, two down, thatched";
+   var price = 100;
+  property = new Property(title, description, price);
 });
 
-  it("has these properties", function() {
+  it("has the property 'title'", function() {
     expect(property.title()).toEqual("Cottage");
   });
+
+  it("has the property 'description'", function() {
+    expect(property.description()).toEqual("Two up, two down, thatched");
+  });
+
+  it("has the property 'price'", function() {
+    expect(property.price()).toEqual(100);
+  });
+
 });
