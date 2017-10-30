7727b819e2a347490be1c48a94bc0d70c38860cd
Pablo <pabloandresvdl@gmail.com>
2017-10-30 17:20:42 +0000

    add two search fields who display what user inputs

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 
-
-
-
+  $(".w3-green").click(function() {
+    var propertyTitle = $(".propertyTitle").val();
+    $("#property_title").text(propertyTitle);
+    var propertyDescription = $(".propertyDescription").val();
+    $("#property_description").text(propertyDescription);
+    event.preventDefault();
+  });
+  
 });
