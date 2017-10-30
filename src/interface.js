8be6573fb1f0167133064f022534bcc84c5712dc
Pablo <pabloandresvdl@gmail.com>
2017-10-30 20:20:36 +0000

    add new field which gets displayed via Jquery

--- src/interface.js
+++ src/interface.js
@@ -1,11 +1,24 @@
+
+
 $(document).ready(function() {
 
   $(".w3-green").click(function() {
+
+
     var propertyTitle = $(".propertyTitle").val();
-    $("#property_title").text(propertyTitle);
     var propertyDescription = $(".propertyDescription").val();
+    var propertyPrice = $(".propertyPrice").val();
+
+var property = new Property(propertyTitle, propertyDescription, propertyPrice);
+    // var property = new Property (propertyTitle, propertyDescription, );
+
+    $("#property_title").text(propertyTitle);
+
     $("#property_description").text(propertyDescription);
+
+    $("#property_price").text(propertyPrice);
+    console.log(property);
     event.preventDefault();
   });
-  
+
 });
